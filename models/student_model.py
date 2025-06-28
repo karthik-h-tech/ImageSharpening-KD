a90656aac7ffc559927ffb55ed7fbb098a7cc2d9
Karthik H <karthikprathibha04@gmail.com>
2025-06-28 11:14:09 +0530

    Clean version with selected files

--- models/student_model.py
+++ models/student_model.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn as nn
 import torchvision.models as models
-import torch.nn.functional as F
 from torchvision import transforms
 
 
@@ -9,14 +8,14 @@ class StudentNet(nn.Module):
     def __init__(self):
         super(StudentNet, self).__init__()
 
-        # Use pretrained MobileNetV2
+        # Pretrained MobileNetV2 as encoder
         mobilenet = models.mobilenet_v2(pretrained=True).features
 
-        # Encoder
-        self.enc1 = mobilenet[0:2]    # Output: 16
-        self.enc2 = mobilenet[2:4]    # Output: 24
-        self.enc3 = mobilenet[4:7]    # Output: 32
-        self.enc4 = mobilenet[7:14]   # Output: 96
+        # Encoder layers
+        self.enc1 = mobilenet[0:2]    # Output: 16 channels
+        self.enc2 = mobilenet[2:4]    # Output: 24 channels
+        self.enc3 = mobilenet[4:7]    # Output: 32 channels
+        self.enc4 = mobilenet[7:14]   # Output: 96 channels
 
         # Bottleneck
         self.bottleneck = nn.Sequential(
@@ -46,9 +45,12 @@ class StudentNet(nn.Module):
             nn.ReLU(inplace=True)
         )
 
+        # Final output layer with Tanh to map output to [-1, 1]
         self.final = nn.Conv2d(8, 3, kernel_size=3, padding=1)
+        self.output_act = nn.Tanh()
 
     def crop_to_match(self, src, tgt):
+        """Crop src tensor to match spatial size of tgt tensor"""
         h_diff = src.size(2) - tgt.size(2)
         w_diff = src.size(3) - tgt.size(3)
         return src[:, :, h_diff // 2: src.size(2) - (h_diff - h_diff // 2),
@@ -76,13 +78,15 @@ class StudentNet(nn.Module):
 
         x = self.up0(x)
         x = self.final(x)
+        x = self.output_act(x)  # Tanh scales to [-1, 1]
+
         return x
 
 
-# Denormalization for visualization
-def denormalize(tensor):
-    return tensor * 0.5 + 0.5
-
-# Normalization for training/testing
+# Normalization for input
 def get_normalize():
-    return transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+    return transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])  # for [-1, 1]
+
+# Denormalization for output
+def denormalize(tensor):
+    return (tensor + 1) / 2  # maps [-1, 1] back to [0, 1]
